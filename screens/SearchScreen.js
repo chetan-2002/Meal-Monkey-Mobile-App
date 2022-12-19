61fa8f93ac9541f9eef497c6b79ef8cd0a843232
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-12-19 16:34:10 +0530

    created search screen

---
+++ screens/SearchScreen.js
@@ -0,0 +1,11 @@
+import { Center, Text } from "native-base";
+
+const SearchScreen = () => {
+  return (
+    <Center>
+      <Text>Search Screen</Text>
+    </Center>
+  );
+};
+
+export default SearchScreen;
