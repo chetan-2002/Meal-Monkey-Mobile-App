2f52f5f3db2b89a1ab674876d546ab38583b8b33
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-12-19 16:28:41 +0530

    new react native project initialized{

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import "react-native-gesture-handler";
+import { NativeBaseProvider } from "native-base";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import LoginSignupNavigator from "./navigation/loginSignupNavigator";
+import CartProvider from "./Context/CartProvider";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <CartProvider>
+      <NativeBaseProvider>
+        <NavigationContainer>
+          <>
+            <LoginSignupNavigator />
+          </>
+        </NavigationContainer>
+      </NativeBaseProvider>
+    </CartProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
